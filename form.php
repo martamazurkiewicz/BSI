dac0039d6a71a6f49fa5f02fad0358b5023e7a8d
martamazurkiewicz <marta@mazurkiewi.cz>
2020-04-23 09:21:38 +0200

    Added html form with php processing

---
+++ form.php
@@ -0,0 +1,167 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Formularz</title>
+	<style>
+		input{
+			border-color: grey;
+		}
+		table{
+			border: 1px solid black;
+			width: 40%;
+			border-radius: 1em;
+			margin-left: 1%;
+			padding-left: 1%;
+			padding-top: 1%;
+			padding-right: 1%;
+			padding-bottom: 1%;
+			border-collapse: separate;
+  			border-spacing: 10px;
+			font-family: Arial, Helvetica, sans-serif;
+		}
+		input[type=text]{
+			width: 90%;
+			height: 1.5em;
+		}
+		input:hover {
+			background-color: lightgray;
+		}
+		</style>
+</head>
+<body>
+	<table>
+		<form action="result.php" method="post">
+		<tr>
+			<th>
+				<label for="name">Imię</label>
+			</th>
+			<th>
+				<input type="text" name="name" placeholder="Imię">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="surname">Nazwisko</label>
+			</th>
+			<th>
+				<input type="text" name="surname" placeholder="Nazwisko">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="street">Ulica</label>
+			</th>
+			<th>
+				<input type="text" name="street" placeholder="Ulica">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="houseNumber">Numer domu</label>
+			</th>
+			<th>
+				<input type="text" name="houseNumber" placeholder="Numer domu">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="flatNumber">Numer mieszkania</label>
+			</th>
+			<th>
+				<input type="text" name="flatNumber" placeholder="Numer mieszkania">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="city">Miasto</label>
+			</th>
+			<th>
+				<input type="text" name="city" placeholder="Miasto">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="gender">Płeć</label>
+			</th>
+			<th>
+				<input type="radio" id="male" name="gender" value="male" checked>
+				<label for="male">Mężczyzna</label>
+		
+				<input type="radio" id="female" name="gender" value="female">
+				<label for="female">Kobieta</label>
+		
+				<input type="radio" id="other" name="gender" value="other">
+				<label for="other">Inna</label>
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="dravingLicence">Czy posiadasz prawo jazdy?</label>
+			</th>
+			<th>
+				<input type="checkbox" name="dravingLicence" value="yes">
+				<label for="dravingLicence">Tak</label>
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="voivodeship">Województwo</label>
+			</th>
+			<th>
+				<select name="voivodeship" style="width: 90%; height: 1.5em;"> 
+					<option value="dolnośląskie">dolnośląskie</option>
+					<option value="kujawsko-pomorskie">kujawsko-pomorskie</option>
+					<option value="lubelskie">lubelskie</option>
+					<option value="lubuskie">lubuskie</option>
+					<option value="łódzkie">łódzkie</option>
+					<option value="małopolskie">małopolskie</option>
+					<option value="mazowieckie">mazowieckie</option>
+					<option value="opolskie">opolskie</option>
+					<option value="podkarpackie">podkarpackie</option>
+					<option value="podlaskie">podlaskie</option>
+					<option value="pomorskie">pomorskie</option>
+					<option value="śląskie">śląskie</option>
+					<option value="świętokrzyskie">świętokrzyskie</option>
+					<option value="warmińsko-mazurskie">warmińsko-mazurskie</option>
+					<option value="wielkopolskie">wielkopolskie</option>
+					<option value="zachodniopomorskie">zachodniopomorskie</option>
+				</select>
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="comments">Uwagi</label>
+			</th>
+			<th>
+				<input type="text" name="comments" style="height:4em;">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="dateofBirth">Data Urodzenia</label>
+			</th>
+			<th>
+				<input type="text" name="dateofBirth" placeholder="dd.mm.rrrr">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<label for="phoneNumber">Numer Telefonu</label>
+			</th>
+			<th>
+				<input type="text" name="phoneNumber" placeholder="123-456-789">
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<input style="width: 10em;height: 2.2em;" type="submit" id="submit" value="Zatwierdź">
+			</th>
+			<th>
+				<input style="width: 10em;height: 2.2em;" type="reset" id="reset" value="Wyczyść">
+			</th>
+		</tr>
+	</form>
+</table>
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Formularz</title>
+	<style>
+		table {
+			border: 1px solid black;
+			width: 40%;
+			border-radius: 1em;
+			margin-left: 1%;
+			padding-left: 1%;
+			padding-top: 1%;
+			padding-right: 1%;
+			padding-bottom: 1%;
+			border-collapse: separate;
+			border-spacing: 10px;
+			font-family: Arial, Helvetica, sans-serif;
+		}
+
+		tr:hover {
+			background-color: lightgray;
+		}
+		.color{
+			background-color: #f5f5f5;
+		}
+
+		.output {
+			width: 60%;
+		}
+	</style>
+</head>
+
+<body>
+
+	<body>
+		<table>
+			<tr class="color">
+				<th>
+					Imię
+				</th>
+				<th class="output">
+					<?php echo $_POST["name"]; ?>
+				</th>
+			</tr>
+			<tr>
+				<th>
+					Nazwisko
+				</th>
+				<th class="output">
+					<?php echo $_POST["surname"]; ?>
+				</th>
+			</tr>
+			<tr class="color">
+				<th>
+					Ulica
+				</th>
+				<th class="output">
+					<?php echo $_POST["street"]; ?>
+				</th>
+			</tr>
+			<tr>
+				<th>
+					Numer domu
+				</th>
+				<th class="output">
+					<?php echo $_POST["houseNumber"]; ?>
+				</th>
+			</tr>
+			<tr class="color">
+				<th>
+					Numer mieszkania
+				</th>
+				<th class="output">
+					<?php echo $_POST["flatNumber"]; ?>
+				</th>
+			</tr>
+			<tr>
+				<th>
+					Miasto
+				</th>
+				<th class="output">
+					<?php echo $_POST["city"]; ?>
+				</th>
+			</tr>
+			<tr class="color">
+				<th>
+					Płeć
+				</th>
+				<th class="output">
+					<?php
+					if ($_POST["gender"] == "male") {
+						echo "mężczyzna";
+					} elseif ($_POST["gender"] == "female") {
+						echo "kobieta";
+					} else {
+						echo "inna";
+					}
+					?>
+				</th>
+			</tr>
+			<tr>
+				<th>
+					Czy posiadasz prawo jazdy?
+				</th>
+				<th class="output">
+					<?php
+					if(isset($_POST['dravingLicence']) && 
+					$_POST['dravingLicence'] == 'yes') {
+						echo "Tak";
+					} else {
+						echo "Nie";
+					}
+					?>
+				</th>
+			</tr>
+			<tr class="color">
+				<th>
+					Województwo
+				</th>
+				<th class="output">
+					<?php echo $_POST["voivodeship"]; ?>
+				</th>
+			</tr>
+			<tr>
+				<th>
+					Uwagi
+				</th>
+				<th class="output">
+					<?php echo $_POST["comments"]; ?>
+				</th>
+			</tr>
+			<tr class="color">
+				<th>
+					Data Urodzenia
+				</th>
+				<th class="output">
+					<?php echo $_POST["dateofBirth"]; ?>
+				</th>
+			</tr>
+			<tr>
+				<th>
+					Numer Telefonu
+				</th>
+				<th class="output">
+					<?php echo $_POST["phoneNumber"]; ?>
+				</th>
+			</tr>
+		</table>
+	</body>
+
+</html>
